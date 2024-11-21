2f744c93cb2287f7b65799c84679b92c9226600d
Spencer Kittleson <spencer.kittleson@nice.com>
2024-11-21 15:53:59 -0800

    add formatter. use new class for whisper

--- index.py
+++ index.py
@@ -1,112 +1,37 @@
 """Whisper app for taking notes through dictation via the cli."""
 
 import sys
-import logging
 import readline
-from queue import Queue
-from threading import Thread
-import sounddevice
-import speech_recognition as sr
-import numpy as np
-from faster_whisper import WhisperModel
-from pydub import AudioSegment
 from rich.console import Console
 import pyperclip
+from transcribe_utils import RecognizerLive
 
-MODEL_SIZE = "small.en"
-logging.getLogger(sounddevice.__name__).setLevel(logging.CRITICAL)
-model = WhisperModel(MODEL_SIZE, device="cpu", compute_type="int8")
-console = Console()
-r = sr.Recognizer()
-audio_queue = Queue()
-text_queue = Queue()
-
-@staticmethod
-def audio_to_file_object(audio: sr.AudioData):
-    """Converts audio stream into object for whisper"""
-
-    audio_segment = AudioSegment(
-        data=audio.get_raw_data(),
-        sample_width=audio.sample_width,
-        frame_rate=audio.sample_rate,
-        channels=1)
-
-    # convert to expected format
-    if audio_segment.frame_rate != 16000: # 16 kHz
-        audio_segment = audio_segment.set_frame_rate(16000)
-    if audio_segment.sample_width != 2:   # int16
-        audio_segment = audio_segment.set_sample_width(2)
-    if audio_segment.channels != 1:       # mono
-        audio_segment = audio_segment.set_channels(1)        
-    arr = np.array(audio_segment.get_array_of_samples())
-    arr = arr.astype(np.float32)/32768.0
-    return arr
-
-def recognize_worker():
-    """background worker to process text in real time"""
-
-    while True:
-        audio = audio_queue.get()  # retrieve the next audio processing job from the main thread
-        if audio is None:
-            break  # stop processing if the main thread is done
-
-        # received audio data, now we'll recognize it
-        try:
-            segments, _ = model.transcribe(audio_to_file_object(audio),
-                        beam_size=5,
-                        word_timestamps=False,
-                        vad_filter=True)
-            segments = list(segments)
-            content = (''.join(segment.text for segment in segments)).strip()
-            if len(content) < 1:
-                continue
-            text_queue.put(content)
-            console.print(content)
-        except sr.UnknownValueError:
-            console.print("could not understand audio")
-        audio_queue.task_done()  # mark the audio processing job as completed in the queue
-
-def audio_thread_listen():
-    """blocking listening thread"""
-    
-    # start a new thread to recognize audio, while this thread focuses on listening
-    recognize_thread = Thread(target=recognize_worker)
-    recognize_thread.daemon = True
-    recognize_thread.start()
-    with sr.Microphone() as source:
-        try:
-            while True:  # repeatedly listen for phrases and put the resulting audio on the audio processing job queue
-                audio_queue.put(r.listen(source, phrase_time_limit=None))
-                # press any key
-        except KeyboardInterrupt:  # allow Ctrl + C to shut down the program
-            pass
-
-    audio_queue.put(None)
-    recognize_thread.join()  # wait for the recognize_thread to actually stop
-    final_content = ''
-    while text_queue.empty() is not True:
-        final_content = final_content + ' ' + text_queue.get()
-    return final_content
 
 def console_user_experience():
     """Primary user experience for taking notes"""
 
-    console.print('Keep speaking until CTRL+C happens then edit the sentence.')
-    note = ''
+    console = Console()
+    console.clear()
+    r = RecognizerLive()
+    r.callback_phrase = console.print
+    note = ""
     while True:
-        user_content = audio_thread_listen()
+        console.print("Keep speaking until CTRL+C happens.")
+        user_content = r.run()
         console.clear()
         try:
             console.print(note, style="dim")
+            console.print("Edit until CTRL+C happens.")
             readline.set_startup_hook(lambda: readline.insert_text(user_content))
-            note = note + ' ' + console.input()
+            note = note + " " + console.input()
             console.clear()
             console.print(note, style="dim")
         except KeyboardInterrupt:
             break
     pyperclip.copy(note)
-    console.print('saved')
+    console.print("saved to clipboard")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     console_user_experience()
     sys.exit(0)
--- transcribe_utils.py
+++ transcribe_utils.py
@@ -8,9 +8,11 @@ import speech_recognition as sr
 import numpy as np
 from faster_whisper import WhisperModel
 from pydub import AudioSegment
+
 logging.getLogger(sounddevice.__name__).setLevel(logging.CRITICAL)
 
-class TranscribeLive:
+
+class RecognizerLive:
     """Transcribe incoming audio chunks to text"""
 
     def __init__(self) -> None:
@@ -18,6 +20,7 @@ class TranscribeLive:
         self._recognizer = sr.Recognizer()
         self._audio_queue = Queue()
         self._text_queue = Queue()
+        self.callback_phrase = None
 
     def _audio_to_file_object(self, audio: sr.AudioData):
         """Converts audio stream into object for whisper"""
@@ -26,17 +29,18 @@ class TranscribeLive:
             data=audio.get_raw_data(),
             sample_width=audio.sample_width,
             frame_rate=audio.sample_rate,
-            channels=1)
+            channels=1,
+        )
 
         # convert to expected format
-        if audio_segment.frame_rate != 16000: # 16 kHz
+        if audio_segment.frame_rate != 16000:  # 16 kHz
             audio_segment = audio_segment.set_frame_rate(16000)
-        if audio_segment.sample_width != 2:   # int16
+        if audio_segment.sample_width != 2:  # int16
             audio_segment = audio_segment.set_sample_width(2)
-        if audio_segment.channels != 1:       # mono
-            audio_segment = audio_segment.set_channels(1)   
+        if audio_segment.channels != 1:  # mono
+            audio_segment = audio_segment.set_channels(1)
         arr = np.array(audio_segment.get_array_of_samples())
-        arr = arr.astype(np.float32)/32768.0
+        arr = arr.astype(np.float32) / 32768.0
         return arr
 
     def _recognize_worker(self):
@@ -47,16 +51,19 @@ class TranscribeLive:
 
             # received audio data, now we'll recognize it
             try:
-                segments, _ = self._model.transcribe(self._audio_to_file_object(audio),
-                            beam_size=5,
-                            word_timestamps=False,
-                            vad_filter=True)
+                segments, _ = self._model.transcribe(
+                    self._audio_to_file_object(audio),
+                    beam_size=5,
+                    word_timestamps=False,
+                    vad_filter=True,
+                )
                 segments = list(segments)
-                content = (''.join(segment.text for segment in segments)).strip()
+                content = ("".join(segment.text for segment in segments)).strip()
                 if len(content) < 1:
                     continue
                 self._text_queue.put(content)
-                # console.print(content)
+                if self.callback_phrase is not None:
+                    self.callback_phrase(content)
             except sr.UnknownValueError:
                 print("could not understand audio")
             self._audio_queue.task_done()  # mark the audio processing job as completed in the queue
@@ -69,19 +76,21 @@ class TranscribeLive:
         with sr.Microphone() as source:
             try:
                 while True:
-                    # repeatedly listen for phrases and put the resulting audio on the audio processing job queue
-                    self._audio_queue.put(self._recognizer.listen(source, phrase_time_limit=None))
+                    # repeatedly listen for phrases then put the resulting audio on the audio job queue
+                    self._audio_queue.put(
+                        self._recognizer.listen(source, phrase_time_limit=None)
+                    )
             except KeyboardInterrupt:  # allow Ctrl + C to shut down the program
                 pass
 
         self._audio_queue.put(None)
         recognize_thread.join()  # wait for the recognize_thread to actually stop
-        final_content = ''
+        final_content = ""
         while self._text_queue.empty() is not True:
-            final_content = final_content + ' ' + self._text_queue.get()
+            final_content = final_content + " " + self._text_queue.get()
         return final_content
-    
-    def run(self):
-        """test"""
 
-        print('placeholder')
+    def run(self):
+        """run instance"""
+
+        return self._audio_thread_listen()
